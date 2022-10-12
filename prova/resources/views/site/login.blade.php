491f2527024a50f9d5022c53055c3885cc252a06
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2022-10-11 22:00:51 -0300

    Criando view e chamando pelo controller

---
+++ prova/resources/views/site/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Prova</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<body>
+    <form method="post" action=""> 
+        <h1>Faça o Login</h1>
+        
+        <p> 
+            <input id="login" name="login" required="required" type="text" placeholder="Usuario" /> 
+        </p>
+        
+        <p> 
+            <input id="senha" name="senha" required="required" type="password" placeholder="****"/>
+        </p>
+
+        <p> 
+            <input type="submit" value="Logar" /> 
+        </p>
+    </form>
+</body>
+</html>
