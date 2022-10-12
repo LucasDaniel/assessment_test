485cf199c94bfc3d6c90758a6719c1f777dc7a69
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2022-10-11 21:46:47 -0300

    Adicionando primeiras toras

--- prova/routes/web.php
+++ prova/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return "Login";
+})->name('site.login');
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('site.login');
 });
